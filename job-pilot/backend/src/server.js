7699b4d0977a756e7b7ae498ac0283347accb173
Jack Goodman <jacksgoodman3@gmail.com>
2025-11-26 11:54:33 +0000

    Dockerfile and .dockerignore added. DB connection added.

---
+++ job-pilot/backend/src/server.js
@@ -0,0 +1,19 @@
+equire('dotenv').config();
+const express = require('express');
+const connectDB = require('./config/db');  // database connect function
+
+const app = express();
+
+// connect to Mongo
+connectDB();
+
+// basic endpoint (optional)
+app.get('/', (req, res) => {
+    res.send("Job Pilot API running");
+});
+
+// start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
